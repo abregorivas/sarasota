eafc6adaff8a0257341dbc467d3918fb1e6d00b0
Roland Abregorivas <abregorivas@gmail.com>
2019-06-09 00:26:05 -0700

    add Visit API

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Family from "../components/Family/Family"
 import Mission from "../components/Mission/Mission"
+import Visit from "../components/Visit/Visit"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Sarasota Christian Fellowship" />
     <Family />
     <Mission />
+    <Visit />
   </Layout>
 )
 
