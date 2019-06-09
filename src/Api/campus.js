a8abd20033b47b1bd4d6322fccd9a2ec6b78960b
Roland Abregorivas <abregorivas@gmail.com>
2019-06-09 02:46:07 -0700

    change campus image location

--- src/Api/campus.js
+++ src/Api/campus.js
@@ -1,39 +1,39 @@
-import eckerd from "../images/campus/eckerd-logo.png";
-import ncf from "../images/campus/ncf-logo.png";
-import rcad from "../images/campus/RCAD_logo.png";
-import scf from "../images/campus/state-college-fl-logo.png";
-import suncoast from "../images/campus/suncoast-logo.png";
-import usf from "../images/campus/usf-logo.png";
+import eckerd from "../images/eckerd-logo.png"
+import ncf from "../images/ncf-logo.png"
+import rcad from "../images/RCAD_logo.png"
+import scf from "../images/state-college-fl-logo.png"
+import suncoast from "../images/suncoast-logo.png"
+import usf from "../images/usf-logo.png"
 
 export const campusInfo = [
   {
     name: "Eckerd College",
     img: eckerd,
-    url: "https://www.eckerd.edu/"
+    url: "https://www.eckerd.edu/",
   },
   {
     name: "New College of Florida",
     img: ncf,
-    url: "https://www.ncf.edu/"
+    url: "https://www.ncf.edu/",
   },
   {
     name: "Ringling college of Art & Design",
     img: rcad,
-    url: "https://www.ringling.edu/"
+    url: "https://www.ringling.edu/",
   },
   {
     name: "State College of Florida",
     img: scf,
-    url: "http://www.scf.edu/"
+    url: "http://www.scf.edu/",
   },
   {
     name: "Suncoast Technical College",
     img: suncoast,
-    url: "https://www.suncoast.edu/"
+    url: "https://www.suncoast.edu/",
   },
   {
     name: "University of South Florida",
     img: usf,
-    url: "http://www.usf.edu/"
-  }
-];
+    url: "http://www.usf.edu/",
+  },
+]
