f6df9dba1325198836c3d8e2c6118b6643a56952
Roland Abregorivas <abregorivas@gmail.com>
2019-06-09 02:28:03 -0700

    Add internship & campus api

---
+++ src/Api/campus.js
@@ -0,0 +1,39 @@
+import eckerd from "../images/campus/eckerd-logo.png";
+import ncf from "../images/campus/ncf-logo.png";
+import rcad from "../images/campus/RCAD_logo.png";
+import scf from "../images/campus/state-college-fl-logo.png";
+import suncoast from "../images/campus/suncoast-logo.png";
+import usf from "../images/campus/usf-logo.png";
+
+export const campusInfo = [
+  {
+    name: "Eckerd College",
+    img: eckerd,
+    url: "https://www.eckerd.edu/"
+  },
+  {
+    name: "New College of Florida",
+    img: ncf,
+    url: "https://www.ncf.edu/"
+  },
+  {
+    name: "Ringling college of Art & Design",
+    img: rcad,
+    url: "https://www.ringling.edu/"
+  },
+  {
+    name: "State College of Florida",
+    img: scf,
+    url: "http://www.scf.edu/"
+  },
+  {
+    name: "Suncoast Technical College",
+    img: suncoast,
+    url: "https://www.suncoast.edu/"
+  },
+  {
+    name: "University of South Florida",
+    img: usf,
+    url: "http://www.usf.edu/"
+  }
+];
---
+++ src/Api/internship.js
@@ -0,0 +1,84 @@
+export const requirements = [
+  {
+    heading: "2 month internship",
+    message:
+      "This is a 24/7 position since it only lasts for a short period of time"
+  },
+  {
+    heading: "Single young man",
+    message: "The nature of the position requires flexibility."
+  },
+  {
+    heading: "$1000/month stipend",
+    message: "It is like having a $250/week expense account"
+  },
+  {
+    heading: "Living arrangements",
+    message:
+      "Willing to live with one of the couples in the church (for free including meals)."
+  },
+  {
+    heading: "Univeral Studios pass (free)",
+    message:
+      "You will be given a free one-day pass to Universal Studios/Islands of Adventure or Wet & Wild (to be used whenever they chose)."
+  },
+  {
+    heading: "Minimum 2 years in the faith",
+    message: "Experience is required to lead and teach."
+  },
+  {
+    heading: "Having beared spiritual fruitful",
+    message: "Instrumental in at least 3-4 conversions to the faith."
+  },
+  {
+    heading: "Evangelistic (a must)",
+    message: "You must be willing to spend entire days out sharing faith"
+  },
+  {
+    heading: "Outgoing and friendly",
+    message: "Good at building friendships with those who are not Christians"
+  },
+  {
+    heading: "Athletic",
+    message:
+      "Able to play sports like basketball, beach volleyball, softball, touch football, soccer in order to meet people."
+  },
+  {
+    heading: "A team player",
+    message: "But still under the leadership of the congregational leaders."
+  },
+  {
+    heading: "Lead a Bible Study",
+    message: "Being able to prepare, coordinate and lead a Bible Study"
+  },
+  {
+    heading: "A strong recommendation.",
+    message:
+      "Coming recommended from a congregation where elders/evangelists know him."
+  },
+  {
+    heading: "A teachable attitude",
+    message:
+      "Open and willing to learn ministry from a former minister/teacher."
+  },
+  {
+    heading: "Public speaking",
+    message: "Willing to preach congregationally at least once a month"
+  },
+  {
+    heading: "Teach the young",
+    message: "Able to teach a Teen midweek Bible class"
+  }
+];
+
+export const jobDescription = {
+  p1:
+    "The goal of this internship program is to train a young man in the fundamentals of ministry, and give him a realistic perspective (expectations, spiritual preparedness, and relationships) on a minister’s personal life. An intern should be prepared to deepen their knowledge and application of scripture in both academic and practical settings.",
+  p2:
+    "This position is not for someone looking for a casual summer vacation to beautiful Sarasota. On the contrary, an intern should be prepared to work hard in helping to build a formidable church in Sarasota.",
+  p3:
+    "Daily activities will include having at least a 1-2 hour quiet time; daily sharing your faith; having breakfast, lunch and/or dinner appointments with non-Christian; and setting up Bible studies with young men in the Sarasota community."
+};
+
+export const paragraph1 =
+  "The goal of this internship program is to train a young man in the fundamentals of ministry, and give him a realistic perspective (expectations, spiritual preparedness, and relationships) on a minister’s personal life. An intern should be prepared to deepen their knowledge and application of scripture in both academic and practical settings.";
