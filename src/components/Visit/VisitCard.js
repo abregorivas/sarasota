05624781601c25dca387c883ccc9cec70da78122
abregorivas <abregorivas@gmail.com>
2019-06-20 16:46:22 -0700

    remove styles

--- src/components/Visit/VisitCard.js
+++ src/components/Visit/VisitCard.js
@@ -1,18 +1,7 @@
 import React from "react"
-import { makeStyles, createStyles } from "@material-ui/styles"
 import { Card, CardMedia, CardContent, Typography } from "@material-ui/core"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    card: {
-      height: "auto",
-      margin: `0 auto`,
-    },
-  })
-)
-
 const VisitCard = props => {
-  const classes = useStyles()
   const { img, time, addr1, addr2, children } = props
 
   return (
