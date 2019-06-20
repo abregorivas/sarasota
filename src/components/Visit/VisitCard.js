940d9eda0b9f789eb4fcbcc39ee56ddc1c363f63
abregorivas <abregorivas@gmail.com>
2019-06-20 16:58:09 -0700

    remove className property

--- src/components/Visit/VisitCard.js
+++ src/components/Visit/VisitCard.js
@@ -5,17 +5,17 @@ const VisitCard = props => {
   const { img, time, addr1, addr2, children } = props
 
   return (
-    <Card className={classes.card} raised>
+    <Card raised>
       {children}
       <CardMedia component="img" image={img} />
       <CardContent>
-        <Typography className={classes.content} variant="body2" component="p">
+        <Typography variant="body2" component="p">
           {time}
         </Typography>
-        <Typography className={classes.content} variant="body2" component="p">
+        <Typography variant="body2" component="p">
           {addr1}
         </Typography>
-        <Typography className={classes.content} variant="body2" component="p">
+        <Typography variant="body2" component="p">
           {addr2}
         </Typography>
       </CardContent>
