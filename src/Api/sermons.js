60588c6a2253fb2406d04fb0ce504068041e3361
abregorivas <abregorivas@gmail.com>
2019-06-20 16:47:05 -0700

    add pdf page count property

--- src/Api/sermons.js
+++ src/Api/sermons.js
@@ -11,6 +11,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "God_Does_not_Accept_Damaged_Goods_6.25.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "Experiencing Intimacy With God",
@@ -18,6 +19,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Experiencing_Intimacy_With_God_7.16.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Feel The Pain",
@@ -25,6 +27,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Feel_the_Pain_7.30.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "It Is Not That Way",
@@ -32,6 +35,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "It_is_not_That_Way_7.9.17",
     hasPdf: true,
+    pdfPages: 8,
   },
   {
     title: "Let Freedom Ring",
@@ -39,6 +43,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Let_Freedom_Ring_7.2.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Obstacles To Intimacy",
@@ -46,6 +51,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Obstacles_to_Intimacy_7.23.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "Consequences",
@@ -53,6 +59,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Consequences_8.20.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Nothing New",
@@ -60,6 +67,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Nothing_New_8.6.17",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "16 Tons And What Do You Get?",
@@ -67,6 +75,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "16_Tons_and_What_Do_You_Get_9.3.17",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "Harvey and Irma Turbulent Couple",
@@ -74,6 +83,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Harvey_and_Irma_Turbulent_Couple_09.17.17",
     hasPdf: false,
+    pdfPages: 0,
   },
   {
     title: "Why Do You Call Me Lord? part 1",
@@ -81,6 +91,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Why_do_you_call_me_Lord_pt1_9.24.17",
     hasPdf: true,
+    pdfPages: 4,
   },
   {
     title: "Why Do You Call Me Lord? part 2",
@@ -88,6 +99,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Why_do_you_call_me_Lord_pt2_10.1.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "What's in your cup? Part 2",
@@ -95,6 +107,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Whats_In_Your_Cup_pt2_10.29.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "What's in your cup? Part 1",
@@ -102,6 +115,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Whats_in_Your_Cup_pt1_10.15.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "It Is Time For An EKG",
@@ -109,6 +123,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Its_time_for_an_EKG_10.8.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Fake Fraud Or The Real Thing?",
@@ -116,6 +131,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Fake_Fraud_or_Real_Thing_11.12.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "The Day After Tomorrow",
@@ -123,6 +139,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "The_Day_After_Tomorrow_11.26.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "The Gods Of Your Ancestors",
@@ -130,6 +147,7 @@ const sermons = [
     author: "Doug Meacham",
     fileName: "The_gods_of_Your_Ancesters_11.5.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "Where Are The Other Guys?",
@@ -137,6 +155,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Where_are_the_other_guys_11.19.17",
     hasPdf: true,
+    pdfPages: 5,
   },
   {
     title: "6 Priorities That Will Transform Your New Year",
@@ -144,6 +163,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "6_Priorities_that_will_transform_your_New_Year_12.31.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "How Do You Know If You Are In The Will Of God?",
@@ -151,6 +171,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "How_Do_You_Know_If_You_Are_In_The_Will_Of_God_12.3.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Shalom, Chaverim! Peace My Friends",
@@ -158,6 +179,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Shalom_chaverim_Peace_My_Friend_12.24.17",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "Skinned Alive",
@@ -165,6 +187,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Skinned_Alive_12.10.17",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Waiting For Christmas",
@@ -172,6 +195,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Waiting_for_Christmas_12.17.17",
     hasPdf: true,
+    pdfPages: 8,
   },
   {
     title: "How Do You Handle A Sucker Punch? Part 1",
@@ -179,6 +203,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "How_Do_You_Handle_A_Sucker_Punch_01.07.18",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "How Do You Handle A Sucker Punch? Part 2",
@@ -186,6 +211,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "How_Do_You_Handle_A_Sucker_Punch__Pt2_01.21.18",
     hasPdf: true,
+    pdfPages: 3,
   },
   {
     title: "How Do You Handle A Sucker Punch? Part 3",
@@ -193,6 +219,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "How_Do_You_Handle_A_Sucker_Punch__Pt3_01.28.18",
     hasPdf: true,
+    pdfPages: 8,
   },
   {
     title: "God's Winning Team",
@@ -200,6 +227,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "God_s Winning Team_2.4.18",
     hasPdf: true,
+    pdfPages: 4,
   },
   {
     title: "Don't Beat A Dead Horse",
@@ -207,6 +235,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Dont_Beat_a_Dead_Horse_02_18_2018",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "From Dumb To Numb",
@@ -214,6 +243,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "From_Dumb_to_Numb_02_11_2018",
     hasPdf: true,
+    pdfPages: 8,
   },
   {
     title: "Can't Run From Shorty",
@@ -221,6 +251,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Cant_Run_From_Shorty_02_24_2018",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "Jumping Jehoshaphat",
@@ -228,6 +259,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "JumpinJehoshaphat_03_04_2018",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "Leadership Lessons From Hezekiah",
@@ -235,6 +267,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Leadership_Lessons_From_Hezekiah_03_11_2018",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "Manasseh Becomes King DOH!!!",
@@ -242,6 +275,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Manasseh_Becomes_King_DOH_03_18_2018",
     hasPdf: true,
+    pdfPages: 8,
   },
   {
     title: "Daniel A Life Without Compromise",
@@ -249,6 +283,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Daniel_A_Life_without_Compromise_03_25_2018",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "Rediscovering God's Remedy For Worry",
@@ -256,6 +291,7 @@ const sermons = [
     author: "Bob Russell",
     fileName: "Rediscovering_Gods_Remedy_For_Worry_04_01_2018",
     hasPdf: false,
+    pdfPages: 0,
   },
   {
     title: "Baptism, Bringing Clarity to an Unclear World",
@@ -263,6 +299,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Baptism_Bringing_Clarity_to_an_Unclear_World_04_08_2018",
     hasPdf: true,
+    pdfPages: 10,
   },
   {
     title: "Baptism! So Which One?",
@@ -270,6 +307,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Baptism_So_Which_One_04_15_2018",
     hasPdf: true,
+    pdfPages: 9,
   },
   {
     title: "Baptism, How Should I Dress?",
@@ -277,6 +315,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Baptism_How_Should_I_Dress_04_22_2018",
     hasPdf: true,
+    pdfPages: 6,
   },
   {
     title: "So where is your parachute?",
@@ -284,6 +323,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "Where_Is_Your_Parachute_04_29_2018",
     hasPdf: true,
+    pdfPages: 7,
   },
   {
     title: "There Is Power In The Blood. Part1",
@@ -291,6 +331,7 @@ const sermons = [
     author: "Ken Zindler",
     fileName: "There_Is_Power_In_The_Blood_pt1_06.03.2018",
     hasPdf: true,
+    pdfPages: 5,
   },
 ]
 
