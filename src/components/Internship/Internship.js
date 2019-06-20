74ab5ca3b8aaf23c0cb8521adbc7baf7d94abf16
abregorivas <abregorivas@gmail.com>
2019-06-20 16:42:23 -0700

    add intro text & withMediaquery for mobile

--- src/components/Internship/Internship.js
+++ src/components/Internship/Internship.js
@@ -3,15 +3,17 @@ import SectionContainer from "../Shared/SectionContainer"
 import SectionHeading from "../Shared/SectionHeading"
 import Requirements from "./Requirements"
 import intershipImg from "../../images/Internship.jpeg"
-import JobDescription from "./JobDescription"
 import DialogContainer from "../Shared/DialogContainer"
-import { Card, CardMedia, Grid, Button } from "@material-ui/core"
+import {  Grid, Button, Typography, useMediaQuery } from "@material-ui/core"
 import { makeStyles, createStyles } from "@material-ui/styles"
+import ImgCard from '../Shared/ImgCard'
+import ContentCard from "../Shared/ContentCard"
+import { useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme =>
   createStyles({
-    media: { height: "auto" },
     button: {
+      marginLeft: theme.spacing(4),
       marginTop: theme.spacing(4),
     },
   })
@@ -22,23 +24,32 @@ const Internship = () => {
   const [open, setOpen] = React.useState(false)
   const handleClickOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div className={classes.root}>
       <SectionContainer id="internship">
-        <Grid container spacing={6}>
-          <Grid item xs={12} sm={5} md={6}>
-            <Card className={classes.card} raised>
-              <CardMedia
-                className={classes.media}
-                component="img"
-                image={intershipImg}
-              />
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={7} md={6}>
+        <Grid container spacing={6}
+              alignItems="center"
+              direction={`${matches}` === 'true' ? 'column-reverse' : 'row'}>
+          <Grid item xs={12} sm={6} >
             <SectionHeading heading="Internship" />
-            <JobDescription />
+            <ContentCard>
+            <Typography
+              className={classes.text}
+              gutterBottom
+              variant="body2"
+              component="p"
+            >
+              The goal of this internship program is to train a young man in the
+              fundamentals of ministry&#44; and give him a realistic perspective
+              &#40;expectations&#44; spiritual preparedness&#44; and
+              relationships&#41; on a minister&#39;s personal life&#46; An intern
+              should be prepared to deepen their knowledge and application of
+              scripture in both academic and practical settings&#46;
+            </Typography>
+            </ContentCard>
             <Button
               className={classes.button}
               variant="contained"
@@ -56,6 +67,10 @@ const Internship = () => {
               <Requirements />
             </DialogContainer>
           </Grid>
+          <Grid item xs={12} sm={6} >
+            <ImgCard imgSrc={intershipImg} />
+          </Grid>
+
         </Grid>
       </SectionContainer>
     </div>
