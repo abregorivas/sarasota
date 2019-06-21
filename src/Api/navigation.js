8f6755137735ab81c3664ec563f7ac5ba4787085
abregorivas <abregorivas@gmail.com>
2019-06-21 01:16:46 -0700

    fix api external bool value

--- src/Api/navigation.js
+++ src/Api/navigation.js
@@ -1,9 +1,9 @@
 export const MainNavRoutes = [
-  { route: "#mission", label: "Mission", external: false },
-  { route: "#sermons", label: "Sermons", external: false },
-  { route: "#visit", label: "Visit", external: false },
-  { route: "#internship", label: "Internship", external: false },
-  { route: "#contact", label: "Contact", external: false },
+  { route: "#mission", label: "Mission", external: true },
+  { route: "#sermons", label: "Sermons", external: true },
+  { route: "#visit", label: "Visit", external: true },
+  { route: "#internship", label: "Internship", external: true },
+  { route: "#contact", label: "Contact", external: true },
   {
     route: "https://www.scgov.net/home",
     label: "Sarasota",
