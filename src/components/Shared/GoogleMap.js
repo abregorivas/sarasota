fd5be7d316cfbe99dce7e9ad60d17f0852edaec0
abregorivas <abregorivas@gmail.com>
2019-06-21 01:16:31 -0700

    refactor to pure component

--- src/components/Shared/GoogleMap.js
+++ src/components/Shared/GoogleMap.js
@@ -1,37 +1,20 @@
-import React from "react"
+import React, { PureComponent } from "react"
+import PropTypes from "prop-types"
 import { Card, CardMedia } from "@material-ui/core"
-import { makeStyles, createStyles } from "@material-ui/styles"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    paper: {
-      position: "absolute",
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(4),
-      outline: "none",
-      top: `50%`,
-      left: `50%`,
-      transform: `translate(-50%, -50%)`,
-    },
-
-    media: {
-      width: 600,
-      height: 400,
-    },
-  })
-)
-
-const GoogleMap = props => {
-  const classes = useStyles()
-  const { url } = props
-  return (
-    <div className={classes.paper}>
-      <Card>
+class GoogleMap extends PureComponent {
+  render() {
+    const { url, classes } = this.props;
+    return (
+      <Card className={classes.card}>
         <CardMedia className={classes.media} component="iframe" image={url} />
       </Card>
-    </div>
-  )
+    )
+  }
+}
+
+GoogleMap.propTypes = {
+  url:PropTypes.string,
 }
 
 export default GoogleMap
