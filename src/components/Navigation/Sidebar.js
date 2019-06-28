9b307c8cd6829d70b058a6c2d7027bfa154cc104
abregorivas <abregorivas@gmail.com>
2019-06-28 14:58:11 -0700

    update Sidebar: menu-items close icon

--- src/components/Navigation/Sidebar.js
+++ src/components/Navigation/Sidebar.js
@@ -2,11 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 import uuidv1 from "uuid"
 import { makeStyles } from "@material-ui/styles"
+import ImportContacts from "@material-ui/icons/ImportContacts"
 
-import { List, Link, ListItem, ListItemText } from "@material-ui/core"
+import {
+  List,
+  Link,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+} from "@material-ui/core"
 
 const useStyles = makeStyles({
   list: { width: 250, textAlign: "left" },
+  link: {
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
 })
 
 const SideBar = props => {
@@ -15,12 +27,30 @@ const SideBar = props => {
   return (
     <List className={classes.list}>
       {menuItems.map(item => (
-        <Link key={uuidv1()} color="inherit" href={item.route}>
+        <Link
+          key={uuidv1()}
+          color="inherit"
+          href={item.route}
+          className={classes.link}
+        >
           <ListItem button>
+            <ListItemIcon>{item.icon}</ListItemIcon>
             <ListItemText primary={item.label} />
           </ListItem>
         </Link>
       ))}
+      <Link
+        color="inherit"
+        href="https://www.ipibooks.com/"
+        className={classes.link}
+      >
+        <ListItem button>
+          <ListItemIcon>
+            <ImportContacts />
+          </ListItemIcon>
+          <ListItemText primary="IPI Books" />
+        </ListItem>
+      </Link>
     </List>
   )
 }
