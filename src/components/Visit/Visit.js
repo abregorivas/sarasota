03b33ead7485b1f2cff16ad27e0db7ab633d1038
abregorivas <abregorivas@gmail.com>
2019-06-29 17:48:11 -0700

    Refactor Visit & visit card update API

--- src/components/Visit/Visit.js
+++ src/components/Visit/Visit.js
@@ -1,56 +1,72 @@
 import React, { useState } from "react"
-import bibleStudy from "../../images/BibleStudy.jpg"
-import bibleReading from "../../images/BibleReading.jpeg"
-import peopleGathered from "../../images/peopleGathered.jpg"
-import Place from "@material-ui/icons/Place"
-import Email from "@material-ui/icons/Email"
 import VisitCard from "./VisitCard"
 import GoogleMap from "../Shared/GoogleMap"
 import { visitusApi } from "../../Api/visitus"
 import SectionContainer from "../Shared/SectionContainer"
 import SectionHeading from "../Shared/SectionHeading"
-import { makeStyles, createStyles } from "@material-ui/styles"
-import { CardHeader, Grid, Modal, IconButton, Typography } from "@material-ui/core"
+import { makeStyles, useTheme } from "@material-ui/styles"
+import uuidv1 from "uuid"
+import { Grid, Modal } from "@material-ui/core"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    root: {
-      flexGrow: 1,
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    height: `calc(100vh - 92px)`,
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#FEFEFE",
+    [theme.breakpoints.down("sm")]: {
+      height: "auto",
     },
-    paper: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-      textAlign: "left",
-      color: theme.palette.text.secondary,
+  },
+  paper: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+    textAlign: "left",
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    fontSize: "12px",
+  },
+  card: {
+    position: "absolute",
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4),
+    outline: "none",
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+  },
+  media: {
+    width: 600,
+    height: 400,
+  },
+  link: {
+    margin: 8,
+    padding: `5px 16px`,
+    textTransform: "uppercase",
+    fontSize: "0.875rem",
+    textDecoration: "none",
+    borderStyle: "solid",
+    borderWidth: `1px`,
+    color: theme.palette.text.primary,
+    borderColor: theme.palette.primary.main,
+    "&:hover": {
+      textDecoration: "none",
+      backgroundColor: theme.palette.primary.main,
+      color: "white",
     },
-    title: {
-      fontSize: "12px",
-    },
-    venueText: {
-      color: theme.palette.text.secondary
-    },
-    card: {
-      position: "absolute",
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(4),
-      outline: "none",
-      top: `50%`,
-      left: `50%`,
-      transform: `translate(-50%, -50%)`,
-    },
-    media: {
-      width: 600,
-      height: 400,
-    },
-  })
-)
+  },
+}))
 
 const Visit = () => {
   const classes = useStyles()
   const [status, setStatus] = useState(false)
   const [map, setMap] = useState("")
-
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down("sm"))
   const handleOpen = x => {
     setStatus(true)
     setMap(x)
@@ -62,88 +78,40 @@ const Visit = () => {
   }
 
   return (
-    <SectionContainer id="visit">
-      <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={status}
-        onClose={() => handleClose(false)}
-      >
-        <GoogleMap url={map} classes={classes}/>
-      </Modal>
+    <div className={classes.root}>
+      <SectionContainer id="visit">
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={status}
+          onClose={() => handleClose(false)}
+        >
+          <GoogleMap url={map} classes={classes} />
+        </Modal>
 
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <SectionHeading
-            heading="Services"
-            subHeading="Come visit us and join the family of believers."
-          />
+        <Grid container spacing={matches ? 2 : 4}>
+          <Grid item xs={12}>
+            <SectionHeading headings={["Come Visit Us"]} />
+          </Grid>
+          {visitusApi.map(item => (
+            <Grid item xs={12} sm={4} key={uuidv1()}>
+              <VisitCard
+                img={item.imgSrc}
+                time={item.time}
+                addr1={item.addr1}
+                addr2={item.addr2}
+                handleAction={() => handleOpen(item.mapUrl)}
+                actionLabel="View Map"
+                title={item.title}
+                subtitle={item.venue}
+                href={item.href}
+                fabIcon={item.icon}
+              />
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <VisitCard
-            img={peopleGathered}
-            time={visitusApi.churchService.time}
-            addr1={visitusApi.churchService.addr1}
-            addr2={visitusApi.churchService.addr2}
-          >
-            <CardHeader
-              disableTypography={true}
-              title={ <Typography varient="body2">{ visitusApi.churchService.title}</Typography> }
-              subheader={ <Typography varient="caption" className={classes.venueText}>{ visitusApi.churchService.venue}</Typography> }
-              action={
-                <IconButton
-                  onClick={() => handleOpen(visitusApi.churchService.mapUrl)}
-                >
-                  <Place />
-                </IconButton>
-              }
-            />
-          </VisitCard>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <VisitCard
-            img={bibleStudy}
-            time={visitusApi.bibleStudy.time}
-            addr1={visitusApi.bibleStudy.addr1}
-            addr2={visitusApi.bibleStudy.addr2}
-          >
-            <CardHeader
-              disableTypography={true}
-              title={ <Typography varient="body2">{ visitusApi.bibleStudy.title}</Typography> }
-              subheader={ <Typography varient="caption" className={classes.venueText}>{ visitusApi.bibleStudy.venue}</Typography> }
-              action={
-                <IconButton
-                  onClick={() => {
-                    handleOpen(visitusApi.bibleStudy.mapUrl)
-                  }}
-                >
-                  <Place />
-                </IconButton>
-              }
-            />
-          </VisitCard>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <VisitCard
-            img={bibleReading}
-            time="Set an appointment"
-            addr1="Come visit us"
-            addr2="Send us an Email "
-          >
-            <CardHeader
-              disableTypography={true}
-              title={ <Typography varient="body2">One on One</Typography> }
-              subheader={ <Typography varient="caption" className={classes.venueText}>Spiritual Guidance</Typography> }
-              action={
-                <IconButton href="#contact">
-                  <Email />
-                </IconButton>
-              }
-            />
-          </VisitCard>
-        </Grid>
-      </Grid>
-    </SectionContainer>
+      </SectionContainer>
+    </div>
   )
 }
 
--- src/components/Visit/VisitCard.js
+++ src/components/Visit/VisitCard.js
@@ -1,14 +1,83 @@
 import React from "react"
-import { Card, CardMedia, CardContent, Typography } from "@material-ui/core"
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Typography,
+  Fab,
+  Link,
+} from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
 
-const VisitCard = props => {
-  const { img, time, addr1, addr2, children } = props
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingBottom: theme.spacing(3),
+    borderRadius: 0,
+    boxShadow: "none",
+    border: `1px solid #eee`,
+    "&:hover": {
+      boxShadow: `0 10px 30px rgba(0, 0, 0, 0.1)`,
+      border: `1px solid transparent`,
+    },
+  },
+  actions: {
+    padding: 0,
+  },
+  content: {
+    textAlign: "center",
+  },
+  button: {
+    margin: theme.spacing(1),
+    borderColor: theme.palette.primary.main,
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+      color: "white",
+    },
+  },
+  icon: {
+    color: "tomato",
+    marginLeft: theme.spacing(2),
+  },
+  fab: {
+    position: "relative",
+    top: 0,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+  },
+}))
+
+const VisitCard = ({
+  img,
+  time,
+  addr1,
+  addr2,
+  handleAction,
+  title,
+  subtitle,
+  fabIcon,
+  href,
+}) => {
+  const classes = useStyles()
 
   return (
-    <Card raised>
-      {children}
+    <Card className={classes.root}>
       <CardMedia component="img" image={img} />
-      <CardContent>
+      <CardActions className={classes.actions}>
+        <Link className={classes.fab} href={href ? href : null}>
+          <Fab color="primary" aria-label="View Map" onClick={handleAction}>
+            {fabIcon}
+          </Fab>
+        </Link>
+      </CardActions>
+      <CardContent className={classes.content}>
+        <Typography variant="subtitle1" component="h6">
+          {title}
+        </Typography>
+        <Typography variant="subtitle2" component="p">
+          {subtitle}
+        </Typography>
+        <br />
         <Typography variant="body2" component="p">
           {time}
         </Typography>
