3464ff79616f0da7b381408e105fbab084cde713
abregorivas <abregorivas@gmail.com>
2019-06-30 02:01:47 -0700

    add invert color prop

--- src/components/Shared/BlockQuote.js
+++ src/components/Shared/BlockQuote.js
@@ -1,39 +1,73 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Typography } from "@material-ui/core"
-import { makeStyles, createStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/styles"
+import FormatQuote from "@material-ui/icons/FormatQuoteRounded"
+import clsx from "clsx"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    quote: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(4),
-      marginTop: theme.spacing(4),
-      marginBottom: theme.spacing(4),
-      color: theme.palette.text.secondary,
-    },
-  })
-)
+const useStyles = makeStyles(theme => ({
+  leftQuoteMark: {
+    position: "relative",
+    top: "42px",
+    height: 42,
+    width: 42,
+    transform: `scale(-1, -1)`,
+  },
+  quote: {
+    paddingLeft: theme.spacing(6),
+    paddingRight: theme.spacing(4),
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(4),
+    maxWidth: 500,
+    fontStyle: "oblique",
+  },
+  inverted: {
+    color: "white",
+  },
+}))
 
 const BlockQuote = props => {
   const classes = useStyles()
-  let { quote } = props
+  let { quote, cite, invert } = props
+
   return (
-    <Typography
-      className={classes.quote}
-      gutterBottom
-      variant="body2"
-      color="secondary"
-      component="blockquote"
-    >
-      <em>{quote}</em>
-    </Typography>
+    <React.Fragment>
+      <FormatQuote
+        className={clsx(classes.leftQuoteMark, {
+          [classes.inverted]: invert,
+        })}
+        color="primary"
+      />
+      <Typography
+        className={clsx(classes.quote, {
+          [classes.inverted]: invert,
+        })}
+        gutterBottom
+        variant="body2"
+        component="blockquote"
+        color="textSecondary"
+      >
+        <em>{quote}</em>
+        <br />
+        {cite && (
+          <Typography
+            className={classes.cite}
+            gutterBottom
+            variant="overline"
+            component="cite"
+          >
+            <em>{cite}</em>
+          </Typography>
+        )}
+      </Typography>
+    </React.Fragment>
   )
 }
 
 BlockQuote.propTypes = {
   quote: PropTypes.string,
   classes: PropTypes.object,
+  invert: PropTypes.string,
 }
 
 export default BlockQuote
