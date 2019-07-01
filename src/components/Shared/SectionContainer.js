988b0dcde06c02be944fa97c53be361d6098e612
abregorivas <abregorivas@gmail.com>
2019-06-30 22:56:32 -0700

    update formating on SectionContainer comp

--- src/components/Shared/SectionContainer.js
+++ src/components/Shared/SectionContainer.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Container } from "@material-ui/core";
-import { makeStyles, createStyles } from "@material-ui/styles";
+import React from "react"
+import PropTypes from "prop-types"
+import { Container } from "@material-ui/core"
+import { makeStyles, createStyles } from "@material-ui/styles"
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -12,25 +12,24 @@ const useStyles = makeStyles(theme =>
       backgroundColor: "transparent",
       borderRadius: 0,
       border: "none",
-
-    }
+    },
   })
-);
+)
 
 const SectionContainer = props => {
-  const classes = useStyles();
-  const { id, children } = props;
+  const classes = useStyles()
+  const { id, children } = props
   return (
     <div id={id} className={classes.root}>
       <Container>{children}</Container>
     </div>
-  );
-};
+  )
+}
 
 SectionContainer.propTypes = {
   id: PropTypes.string,
   children: PropTypes.node.isRequired,
-  classes: PropTypes.object
-};
+  classes: PropTypes.object,
+}
 
-export default SectionContainer;
+export default SectionContainer
