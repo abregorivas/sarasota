3490b9f1f2c876ddca018f504ebbfd82b0eb199c
abregorivas <abregorivas@gmail.com>
2019-06-30 22:15:13 -0700

    Update styles/layout on Internship

--- src/components/Internship/Internship.js
+++ src/components/Internship/Internship.js
@@ -1,76 +1,131 @@
 import React from "react"
 import SectionContainer from "../Shared/SectionContainer"
 import SectionHeading from "../Shared/SectionHeading"
-import Requirements from "./Requirements"
 import intershipImg from "../../images/Internship.jpeg"
 import DialogContainer from "../Shared/DialogContainer"
-import {  Grid, Button, Typography, useMediaQuery } from "@material-ui/core"
-import { makeStyles, createStyles } from "@material-ui/styles"
-import ImgCard from '../Shared/ImgCard'
-import ContentCard from "../Shared/ContentCard"
-import { useTheme } from '@material-ui/core/styles';
+import {
+  Grid,
+  Button,
+  Typography,
+  Divider,
+  useMediaQuery,
+} from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
+import ImgCard from "../Shared/ImgCard"
+import { useTheme } from "@material-ui/core/styles"
+import RequirementList from "./RequirementsList"
+import { requirements } from "../../Api/internship"
+import Campus from "./Campus"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    button: {
-      marginLeft: theme.spacing(4),
-      marginTop: theme.spacing(4),
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    height: `calc(100vh - 92px)`,
+    backgroundColor: "#FEFEFE",
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      height: "auto",
     },
-  })
-)
+  },
+  button: {
+    border: `1px solid #F5B733`,
+    backgroundColor: "transparent",
+    fontWeight: "bold",
+    borderRadius: 0,
+    paddingRight: theme.spacing(3),
+    paddingLeft: theme.spacing(3),
+    marginTop: theme.spacing(2),
+    "&:hover": {
+      backgroundColor: "#f5b733",
+    },
+  },
+  content: {
+    maxWidth: 500,
+    color: theme.palette.text.secondary,
+    position: "relative",
+  },
+}))
 
 const Internship = () => {
   const classes = useStyles()
   const [open, setOpen] = React.useState(false)
   const handleClickOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down("sm"))
 
   return (
-    <div className={classes.root}>
-      <SectionContainer id="internship">
-        <Grid container spacing={6}
-              alignItems="center"
-              direction={`${matches}` === 'true' ? 'column-reverse' : 'row'}>
-          <Grid item xs={12} md={6} >
-            <SectionHeading heading="Internship" />
-            <ContentCard>
+    <div className={classes.root} id="internship">
+      <SectionContainer>
+        <Grid
+          container
+          spacing={8}
+          alignItems="center"
+          direction={`${matches}` === "true" ? "column-reverse" : "row"}
+        >
+          <Grid container item xs={12} md={6}>
+            <SectionHeading headings={["Internship"]} />
+
             <Typography
-              className={classes.text}
-              gutterBottom
+              className={classes.content}
               variant="body2"
-              component="p"
+              gutterBottom={true}
             >
               The goal of this internship program is to train a young man in the
               fundamentals of ministry&#44; and give him a realistic perspective
               &#40;expectations&#44; spiritual preparedness&#44; and
-              relationships&#41; on a minister&#39;s personal life&#46; An intern
-              should be prepared to deepen their knowledge and application of
-              scripture in both academic and practical settings&#46;
+              relationships&#41; on a minister&#39;s personal life&#46; An
+              intern should be prepared to deepen their knowledge and
+              application of scripture in both academic and practical
+              settings&#46;
             </Typography>
-            </ContentCard>
+            <br />
             <Button
               className={classes.button}
-              variant="contained"
+              variant="outlined"
               onClick={handleClickOpen}
-              color="primary"
             >
               View Requirements
             </Button>
-
-            <DialogContainer
-              title="Intership Requirements"
-              open={open}
-              handleClose={() => handleClose()}
-            >
-              <Requirements />
-            </DialogContainer>
           </Grid>
-          <Grid item xs={12} md={6} >
+          <DialogContainer
+            title="Intership Requirements"
+            open={open}
+            handleClose={() => handleClose()}
+          >
+            <Grid container style={{ marginTop: "80px" }}>
+              <Grid item xs={12} sm={6}>
+                <RequirementList list={requirements.slice(0, 8)} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <RequirementList list={requirements.slice(8, 16)} />
+              </Grid>
+            </Grid>
+            <br />
+            <br />
+            <Divider />
+            <br />
+            <br />
+            <Grid container spacing={1} wrap="wrap">
+              <Grid item xs={12}>
+                <Typography variant="subtitle1">Campus Locations</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle2">
+                  You will have the opportunity to sharing your faith at each of
+                  these campuses.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={1} wrap="wrap">
+              <Campus />
+            </Grid>
+          </DialogContainer>
+
+          <Grid item xs={12} md={6}>
             <ImgCard imgSrc={intershipImg} />
           </Grid>
-
         </Grid>
       </SectionContainer>
     </div>
