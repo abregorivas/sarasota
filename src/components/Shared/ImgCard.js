e6f6d67fb6bf4292e55e311c8c67a27f67e8992e
abregorivas <abregorivas@gmail.com>
2019-06-30 21:50:51 -0700

    update styles imgCard

--- src/components/Shared/ImgCard.js
+++ src/components/Shared/ImgCard.js
@@ -1,24 +1,21 @@
 import React from "react"
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types"
 import { Card, CardMedia } from "@material-ui/core"
-import { createStyles, makeStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/styles"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    card: {
-      margin: theme.spacing(2),
-    },
-  }))
+const useStyles = makeStyles({
+  root: {
+    borderRadius: 0,
+    height: "100%",
+  },
+})
 
-const ImgCard = ({imgSrc}) => {
-const classes = useStyles()
+const ImgCard = ({ imgSrc }) => {
+  const classes = useStyles()
   return (
-      <Card className={classes.card} raised>
-        <CardMedia
-          component="img"
-          src={imgSrc}
-        />
-      </Card>
+    <Card className={classes.root} raised>
+      <CardMedia component="img" src={imgSrc} />
+    </Card>
   )
 }
 
