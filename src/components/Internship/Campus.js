4297c05ab61e2a82f22f7319d54cbd8fa836ac00
abregorivas <abregorivas@gmail.com>
2019-06-30 21:29:43 -0700

    Refactor Campus Comp

--- src/components/Internship/Campus.js
+++ src/components/Internship/Campus.js
@@ -1,47 +1,41 @@
 import React from "react"
 import { Card, CardMedia, Grid } from "@material-ui/core"
-import { campusInfo } from "../../Api/campus"
-import uuidv1 from "uuid"
-import { makeStyles, createStyles } from "@material-ui/styles"
+import { campusInfo } from "../../Api/campusApi"
+import uuid from "uuid"
+import { makeStyles } from "@material-ui/styles"
 
-const useStyles = makeStyles(theme =>
-  createStyles({
-    root: {
-      display: "flex",
-      flexWrap: "wrap",
-      justifyContent: "space-around",
-      overflow: "hidden",
-      backgroundColor: theme.palette.background.paper,
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginTop: theme.spacing(3),
+    height: "auto",
+    [theme.breakpoints.down("lg")]: {
+      height: 120,
     },
-    gridList: {},
-    icon: {
-      color: "rgba(255, 255, 255, 0.54)",
-    },
-    card: { boxShadow: "none", height: 100 },
-    media: {},
-  })
-)
+    display: "flex",
+    alignItems: "center",
+    margin: theme.spacing(2),
+  },
+  media: { padding: theme.spacing(2) },
+}))
 
-const Campus = props => {
+const Campus = () => {
   const classes = useStyles()
   return (
-    <div className={classes.root}>
-      <Grid container spacing={6}>
-        {campusInfo.map(item => (
-          <Grid key={uuidv1()} item xs={6} sm={3} md={2}>
-            <a href={item.url} target="_blank" rel="noopener noreferrer">
-              <Card className={classes.card}>
-                <CardMedia
-                  component="img"
-                  className={classes.media}
-                  image={item.img}
-                />
-              </Card>
-            </a>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
+    <React.Fragment>
+      {campusInfo.map(item => (
+        <Grid item xs={6} sm={4} lg={2} key={uuid()}>
+          <a href={item.url} target="_blank" rel="noopener noreferrer">
+            <Card raised className={classes.card}>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={item.img}
+              />
+            </Card>
+          </a>
+        </Grid>
+      ))}
+    </React.Fragment>
   )
 }
 
