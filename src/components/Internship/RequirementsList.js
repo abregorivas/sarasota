8dc05aeba45d7058b881898696186b4052c8c6e1
abregorivas <abregorivas@gmail.com>
2019-06-30 23:00:16 -0700

    Add bullet point to RequirementsList

--- src/components/Internship/RequirementsList.js
+++ src/components/Internship/RequirementsList.js
@@ -1,14 +1,46 @@
 import React from "react"
-import { List, ListItem, ListItemText } from "@material-ui/core"
-import uuidv1 from "uuid"
+import { Avatar, List, ListItem, ListItemText } from "@material-ui/core"
+import uuid from "uuid"
+import { makeStyles } from "@material-ui/styles"
+import Typography from "@material-ui/core/Typography"
+import ListItemAvatar from "@material-ui/core/ListItemAvatar"
+
+const useStyles = makeStyles({
+  heading: {
+    maxWidth: 500,
+    fontWeight: "bold",
+  },
+  listItem: {
+    maxWidth: 500,
+    "&:hover": {
+      "&:hover": {
+        boxShadow: `0 10px 30px rgba(0, 0, 0, 0.1)`,
+        border: `1px solid transparent`,
+      },
+    },
+  },
+})
 
 const RequirementsList = props => {
+  const classes = useStyles()
   const { list } = props
   return (
-    <List dense>
+    <List>
       {list.map(item => (
-        <ListItem key={uuidv1()}>
-          <ListItemText primary={item.heading} secondary={item.message} />
+        <ListItem key={uuid()} className={classes.listItem}>
+          <ListItemAvatar>
+            <Avatar>></Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            disableTypography={true}
+            className={classes.heading}
+            primary={
+              <Typography variant="subtitle1">{item.heading}</Typography>
+            }
+            secondary={
+              <Typography variant="subtitle2">{item.message}</Typography>
+            }
+          />
         </ListItem>
       ))}
     </List>
