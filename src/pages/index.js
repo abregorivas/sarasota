1f94a5b946f50fb6279b54c5ea65f7512a90fe9a
abregorivas <abregorivas@gmail.com>
2019-06-30 22:15:58 -0700

    change order on Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/Hero/Hero"
 import Family from "../components/Family/Family"
 import Mission from "../components/Mission/Mission"
 import Visit from "../components/Visit/Visit"
+import Sermon from "../components/Sermons/Sermon"
 import Internship from "../components/Internship/Internship"
 import Contact from "../components/Contact/Contact"
-import Sermon from "../components/Sermons/Sermons"
-import Hero from "../components/Hero/Hero"
 
 const IndexPage = () => (
   <Layout>
