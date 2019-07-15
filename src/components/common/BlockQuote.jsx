ae3d0db3ee78743ed16a6f653ff932e53850be74
abregorivas <abregorivas@gmail.com>
2019-07-15 00:37:36 -0700

    create common components

---
+++ src/components/common/BlockQuote.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import FormatQuote from '@material-ui/icons/FormatQuoteRounded';
+import clsx from 'clsx';
+
+const useStyles = makeStyles(theme => ({
+  leftQuoteMark: {
+    position: 'relative',
+    top: '42px',
+    height: 42,
+    width: 42,
+    transform: `scale(-1, -1)`
+  },
+  quote: {
+    paddingLeft: theme.spacing(6),
+    paddingRight: theme.spacing(4),
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(4),
+    maxWidth: 500,
+    fontStyle: 'oblique'
+  },
+  inverted: {
+    color: 'white'
+  }
+}));
+
+const BlockQuote = ({ quote, cite, invert }) => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <FormatQuote
+        className={clsx(classes.leftQuoteMark, {
+          [classes.inverted]: invert
+        })}
+        color="primary"
+      />
+      <Typography
+        className={clsx(classes.quote, {
+          [classes.inverted]: invert
+        })}
+        gutterBottom
+        variant="body2"
+        component="blockquote"
+        color="textSecondary"
+      >
+        <em>{quote}</em>
+        <br />
+        {cite && (
+          <Typography
+            className={classes.cite}
+            gutterBottom
+            variant="overline"
+            component="cite"
+          >
+            <em>{cite}</em>
+          </Typography>
+        )}
+      </Typography>
+    </React.Fragment>
+  );
+};
+
+BlockQuote.propTypes = {
+  quote: PropTypes.string,
+  classes: PropTypes.object,
+  invert: PropTypes.string
+};
+
+export default BlockQuote;
---
+++ src/components/common/SectionHeading.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/styles';
+import uuid from 'uuid';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    heading: {
+      color: theme.palette.text.primary,
+      fontWeight: 'bold'
+    }
+  })
+);
+
+const SectionHeading = ({ headings, align, children }) => {
+  const classes = useStyles();
+
+  // noinspection JSRemoveUnnecessaryParentheses
+  return (
+    <div className={classes.root}>
+      {headings.map(heading => (
+        <Typography
+          key={uuid()}
+          gutterBottom
+          variant="h3"
+          className={classes.heading}
+          component="h3"
+          align={align}
+        >
+          {heading}
+        </Typography>
+      ))}
+      {children}
+    </div>
+  );
+};
+
+SectionHeading.propTypes = {
+  heading: PropTypes.string,
+  subHeading: PropTypes.string,
+  align: PropTypes.string
+};
+
+export default SectionHeading;
