2cd38aeaa0faa2582f362668081c9a894de0edfa
abregorivas <abregorivas@gmail.com>
2019-07-15 01:27:10 -0700

    add cmaps to pdfviewer

--- src/components/Sermons/PDFViewer.jsx
+++ src/components/Sermons/PDFViewer.jsx
@@ -1,52 +1,40 @@
-import React, { useState } from "react"
-import PropTypes from "prop-types"
-import {
-  Card,
-  CardContent,
-  Typography,
-  Button,
-  MobileStepper,
-} from "@material-ui/core"
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardContent, Button, MobileStepper } from '@material-ui/core';
 import { Document, Page } from 'react-pdf/dist/entry.parcel';
-// import { pdfjs } from 'react-pdf';
-// import { Document, Page } from "react-pdf"
-import { makeStyles, useTheme } from "@material-ui/styles"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft"
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight"
-// import { Document, Page } from 'react-pdf/dist/entry.webpack';
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { pdfjs } from 'react-pdf';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
-
-// import spdf from "simple-react-pdf";
 const useStyles = makeStyles({
   card: {
     borderRadius: 0,
-    boxShadow: "none",
-    overflow: "auto",
-    height: `calc(100vh - 200px)`,
+    boxShadow: 'none',
+    overflow: 'auto',
+    height: `calc(100vh - 200px)`
   },
   media: {},
   progress: {
-    position: "relative",
+    position: 'relative',
     top: 200,
-    left: 200,
+    left: 200
   },
   root: {
-    background: "#eceff1",
-  },
-
-})
+    background: '#eceff1'
+  }
+});
 
 const PDFViewer = props => {
-  const classes = useStyles()
-  const { sermon } = props
-  const theme = useTheme()
-  const [activeStep, setActiveStep] = useState(0)
+  const classes = useStyles();
+  const { sermon } = props;
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
 
-  const handleNext = () => setActiveStep(prevActiveStep => prevActiveStep + 1)
+  const handleNext = () => setActiveStep(prevActiveStep => prevActiveStep + 1);
 
-  const handleBack = () => setActiveStep(prevActiveStep => prevActiveStep - 1)
+  const handleBack = () => setActiveStep(prevActiveStep => prevActiveStep - 1);
 
   // useEffect(() => {
   //
@@ -67,7 +55,7 @@ const PDFViewer = props => {
             disabled={activeStep === sermon.pdfPages - 1}
           >
             Next
-            {theme.direction === "rtl" ? (
+            {theme.direction === 'rtl' ? (
               <KeyboardArrowLeft />
             ) : (
               <KeyboardArrowRight />
@@ -76,7 +64,7 @@ const PDFViewer = props => {
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === "rtl" ? (
+            {theme.direction === 'rtl' ? (
               <KeyboardArrowRight />
             ) : (
               <KeyboardArrowLeft />
@@ -88,8 +76,10 @@ const PDFViewer = props => {
       <Card className={classes.card}>
         <CardContent>
           <Document
-            // options ={{cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
-            //   cMapPacked: true,}}
+            options={{
+              cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+              cMapPacked: true
+            }}
             file={`sermons/pdf/${sermon.fileName}.pdf`}
             loading={
               <CircularProgress disableShrink className={classes.progress} />
@@ -104,11 +94,11 @@ const PDFViewer = props => {
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
 PDFViewer.propTypes = {
-  sermon: PropTypes.object,
-}
+  sermon: PropTypes.PropTypes.node
+};
 
-export default PDFViewer
+export default PDFViewer;
