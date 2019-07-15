15c7c8813db13e9745829f224e38b367bf547df1
abregorivas <abregorivas@gmail.com>
2019-07-15 00:40:13 -0700

    create sermon components

---
+++ src/components/Sermons/PDFViewer.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  MobileStepper,
+} from "@material-ui/core"
+import { Document, Page } from 'react-pdf/dist/entry.parcel';
+// import { pdfjs } from 'react-pdf';
+// import { Document, Page } from "react-pdf"
+import { makeStyles, useTheme } from "@material-ui/styles"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft"
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight"
+// import { Document, Page } from 'react-pdf/dist/entry.webpack';
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+// import spdf from "simple-react-pdf";
+const useStyles = makeStyles({
+  card: {
+    borderRadius: 0,
+    boxShadow: "none",
+    overflow: "auto",
+    height: `calc(100vh - 200px)`,
+  },
+  media: {},
+  progress: {
+    position: "relative",
+    top: 200,
+    left: 200,
+  },
+  root: {
+    background: "#eceff1",
+  },
+
+})
+
+const PDFViewer = props => {
+  const classes = useStyles()
+  const { sermon } = props
+  const theme = useTheme()
+  const [activeStep, setActiveStep] = useState(0)
+
+  const handleNext = () => setActiveStep(prevActiveStep => prevActiveStep + 1)
+
+  const handleBack = () => setActiveStep(prevActiveStep => prevActiveStep - 1)
+
+  // useEffect(() => {
+  //
+  // },[])
+
+  return (
+    <div>
+      <MobileStepper
+        className={classes.root}
+        steps={sermon.pdfPages}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === sermon.pdfPages - 1}
+          >
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+      <Card className={classes.card}>
+        <CardContent>
+          <Document
+            // options ={{cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+            //   cMapPacked: true,}}
+            file={`sermons/pdf/${sermon.fileName}.pdf`}
+            loading={
+              <CircularProgress disableShrink className={classes.progress} />
+            }
+          >
+            <Page
+              pageNumber={activeStep + 1}
+              scale={1.3}
+              loading="Please Wait"
+            />
+          </Document>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+PDFViewer.propTypes = {
+  sermon: PropTypes.object,
+}
+
+export default PDFViewer
---
+++ src/components/Sermons/Sermon.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react"
+import SectionContainer from "../common/SectionContainer"
+import { Avatar, Grid, Button, Slide, Dialog, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
+import Search from "./Search"
+import SectionHeading from "../common/SectionHeading"
+import Benefit from "./Benefit"
+import kenZindler from "../../assets/images/kenZindler.jpg"
+import ProfileCard from "./ProfileCard"
+import microphone from '../../assets/images/microphone.jpg'
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: '#F8F8F8',
+    [theme.breakpoints.down("sm")]: {
+      height: "auto",
+    },
+  },
+  avatar: {
+    position: "absolute",
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+    border: `5px solid white`,
+    height: 160,
+    width: 160,
+  },
+  content: {
+    maxWidth: 500,
+    color: theme.palette.text.secondary,
+    position: "relative",
+  },
+  button: {
+    border: `1px solid #F5B733`,
+    backgroundColor: "white",
+    fontWeight: "bold",
+    borderRadius: 0,
+    paddingRight: theme.spacing(3),
+    paddingLeft: theme.spacing(3),
+    marginTop: theme.spacing(2),
+    "&:hover": {
+      backgroundColor: "#F5B733",
+    },
+  },
+}))
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />
+})
+
+const Sermon = () => {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+  const handleClickOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <div className={classes.root}>
+      <SectionContainer id="sermons">
+        <Grid container spacing={8} alignItems="center">
+          <Grid item xs={12} md={6} style={{ position: "relative" }}>
+            <ProfileCard
+              title="Ken Zindler"
+              subtitle="Lead Evangelist"
+              fabIcon={
+                <Avatar
+                  className={classes.avatar}
+                  alt="Ken Zindler"
+                  src={kenZindler}
+                />
+              }
+              img={microphone}
+            />
+
+            <Dialog
+              fullScreen
+              open={open}
+              onClose={handleClose}
+              TransitionComponent={Transition}
+            >
+              <Search handleClose={handleClose} />
+            </Dialog>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <SectionHeading headings={["Audio For", "The Soul"]}>
+              <Typography variant="body2" className={classes.content}>
+                We are making our weekly sermons, along with the PDF notes (to follow along with the sermon)
+                available to the general public. For those in the ministry, the sermon notes are not copyrighted so
+                you are free to download the outlines and use them any way you choose. Our goal in providing
+                these services is to further spread the gospel of Jesus (free of charge). We hope that these lessons
+                will strengthen your spiritual walk and provide solid, doctrinal guidance for all those seeking to
+                draw closer to the Lord.
+              </Typography>
+            </SectionHeading>
+            <br />
+            <Benefit />
+            <Button
+              className={classes.button}
+              variant="outlined"
+              onClick={handleClickOpen}
+            >
+              Listen Now
+            </Button>
+          </Grid>
+        </Grid>
+      </SectionContainer>
+    </div>
+  )
+}
+
+export default Sermon
