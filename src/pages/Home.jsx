bbc4bca98567fe5c23189c088c4fa755f5ab852b
abregorivas <abregorivas@gmail.com>
2019-07-15 00:36:35 -0700

    create home comp

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Family from '../components/Family';
+import Mission from '../components/Mission';
+import Visit from '../components/Visit';
+import Sermon from '../components/Sermons';
+import Internship from '../components/Internship';
+import Contact from '../components/Contact';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Family />
+      <Mission />
+      <Visit />
+      <Sermon />
+      <Internship />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
