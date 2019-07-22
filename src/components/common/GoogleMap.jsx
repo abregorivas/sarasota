aa4569b6bca3e406465ec5f5e924f7a259b75d72
abregorivas <abregorivas@gmail.com>
2019-07-21 20:13:06 -0700

    refactor googlemaps to purecomp & withStyles

--- src/components/common/GoogleMap.jsx
+++ src/components/common/GoogleMap.jsx
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardMedia } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { withStyles, createStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles(theme => ({
-  modalCard: {
-    position: 'absolute',
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(4),
-    outline: 'none',
-    top: `50%`,
-    left: `50%`,
-    transform: `translate(-50%, -50%)`
-  },
-  modalMedia: {
-    width: 600,
-    height: 400,
-    [theme.breakpoints.down('xs')]: {
-      maxWidth: 360,
-      maxHeight: 260
+const styles = theme =>
+  createStyles({
+    modalCard: {
+      position: 'absolute',
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(4),
+      outline: 'none',
+      top: `50%`,
+      left: `50%`,
+      transform: `translate(-50%, -50%)`
+    },
+    modalMedia: {
+      width: 600,
+      height: 400,
+      [theme.breakpoints.down('xs')]: {
+        maxWidth: 360,
+        maxHeight: 260
+      }
     }
-  }
-}));
+  });
 
-const GoogleMap = ({ url }) => {
-  const classes = useStyles();
-  return (
-    <Card className={classes.modalCard}>
-      <CardMedia
-        className={classes.modalMedia}
-        component="iframe"
-        image={url}
-      />
-    </Card>
-  );
-};
+class GoogleMap extends PureComponent {
+  render() {
+    const { url, classes } = this.props;
+    return (
+      <Card className={classes.modalCard}>
+        <CardMedia
+          className={classes.modalMedia}
+          component="iframe"
+          image={url}
+        />
+      </Card>
+    );
+  }
+}
 
 GoogleMap.propTypes = {
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
+  classes: PropTypes.shape({
+    modalCard: PropTypes.string,
+    modalMedia: PropTypes.string
+  }).isRequired
 };
 
-export default GoogleMap;
+export default withStyles(styles)(GoogleMap);
